d8d419547703f725ff6422828f5b9d6d601f94aa
Alexandre Daoud <alex.daoud@mac.com>
2023-11-21 14:42:23 +0000

    Update

--- lighthouse/runner.py
+++ lighthouse/runner.py
@@ -1,5 +1,6 @@
 # Standard library modules
 from collections import namedtuple
+from concurrent.futures import ProcessPoolExecutor
 from datetime import datetime
 from functools import reduce
 from itertools import product
@@ -26,6 +27,7 @@ BASE_TIMINGS = [
     'estimated-input-latency',
     'time-to-first-byte',
 ]
+DEFAULT_CHROME_FLAGS = "--headless --no-sandbox"
 
 class LighthouseRunner:
     """
@@ -39,101 +41,110 @@ class LighthouseRunner:
     def __init__(
         self,
         url:str,
+        config:str='default',
         form_factor:str='mobile',
         quiet:bool=True,
-        chrome_flags: str='--headless --no-sandbox',
+        chrome_flags: str=DEFAULT_CHROME_FLAGS,
         additional_settings:List[str]=[],
         timings:List[str]=BASE_TIMINGS,
         output_type:str='none',
         output_dir:str='./'
     ) -> None :
-        """ 
+        """
         Initialize a new LighthousePerformanceTest object.
-        
+
         Args:
             url (str): The URL to test.
-            
+
+            config (str, optional): The Lighthouse config to use for the test.
+
             form_factor (str, optional): The form factor to use for the test. Either 'mobile' or 'desktop'.
                 Defaults to 'mobile'.
-                
+
             quiet (bool, optional): Whether to suppress output to stdout. Defaults to True.
 
             chrome_flags (str, optional): A string of flags used to configure the Chrome runner.
-            
+
             additional_settings (list, optional): A list of additional parameters to use for the test.
                 Defaults to an empty list.
-                
+
             timings (list, optional): A list of performance metrics to measure during the test.
                 Defaults to BASE_TIMINGS, which includes several common metrics.
-                
+
             output_type (str, optional): The type of output to generate. Possible values are 'none','both',
                 'json', and 'html'. Defaults to 'none'.
-                
+
             output_dir (str, optional): The directory where output files should be written.
                 Defaults to the current directory.
-        
+
         Raises:
             AssertionError: If form_factor or output_type are not valid values.
-            
+
         """
 
         # Check inputs
         assert form_factor in ['mobile', 'desktop'], f"Invalid form_factor: {form_factor}. Allowed values are 'mobile' or 'desktop'."
         assert output_type in ['json','html','both','none'], f"Invalid output_type: {output_type}. Allowed values are 'json', 'html', 'both', or 'none'."
-        
+
         # Generate a unique report name based on the current date and time, plus a random UUID for report path
         self.report_name = str(datetime.now()).replace(" ","_") +"_"+ uuid4().hex
         self.report_path = os.path.join("/tmp",self.report_name)
-        
+
         # Set the timings, output directory, and output type for the test
         self.timings = timings
         self.output_dir = output_dir
         self.output_type = output_type.lower().strip()
-        
+
         # Run the performance test with the given settings and get the report data
         self._run(url, form_factor, quiet, chrome_flags, additional_settings)
         self.report = self._get_report()
-        
+
         # Clean up any temporary files
         self._clean()
 
     def _run(
         self,
         url:str,
+        preset:str='default',
         form_factor:str='mobile',
         quiet:bool=True,
-        chrome_flags: str='--headless --no-sandbox',
+        chrome_flags: str=DEFAULT_CHROME_FLAGS,
         additional_settings:List[str]=[]
     ) -> None:
         """
         Run a performance test using the Lighthouse CLI.
-        
+
         Args:
             url (str): The URL to test.
-            
+
+            preset (str, optional): The Lighthouse config to use for the test.
+
             form_factor (str, optional): The form factor to use for the test. Either 'mobile' or 'desktop'.
                 Defaults to 'mobile'.
-                
+
             quiet (bool, optional): Whether to suppress output to stdout. Defaults to True.
-            
+
+            chrome_flags (str, optional): A string of flags used to configure the Chrome runner.
+
             additional_settings (list, optional): A list of additional parameters to use for the test.
                 Defaults to an empty list.
 
         Raises:
             RuntimeError: If there is an error running the performance test using the Lighthouse CLI, \
             such as a missing or invalid URL, an invalid form factor, a problem with the output path, or an issue with the Lighthouse CLI itself.
-            
+
         """
 
         try:
-            
+
             # Construct the Lighthouse CLI command
             command = [
                 'lighthouse',
                 url,
                 '--quiet' if quiet else '',
                 '--chrome-flags="{0}"'.format(chrome_flags),
-                '--preset={0}'.format(form_factor),
+                '--preset={0}'.format(preset),
+                '--form-factor={0}'.format(form_factor),
                 '--output=json',
                 '--output=html',
                 '--output-path={0}'.format(self.report_path),
@@ -155,21 +166,21 @@ class LighthouseRunner:
         Returns:
             LighthouseReport or None: A LighthouseReport object containing the report data, or None if the report 
             could not be loaded.
-        
+
         Raises:
             Exception: If there is an error loading the report data from the JSON file.
         """
-        
+
         # Copy the HTML report to the output directory if output_type is 'html' or 'both'
         if self.output_type in ["html","both"]:
             os.makedirs(self.output_dir,exist_ok=True)
             shutil.copy(self.report_path+".report.html", os.path.join(self.output_dir,self.report_name+".html"))
-            
+
         # Copy the JSON report to the output directory if output_type is 'json' or 'both'
         if self.output_type in ["json","both"]:
             os.makedirs(self.output_dir,exist_ok=True)
             shutil.copy(self.report_path+".report.json", os.path.join(self.output_dir,self.report_name+".json"))
-            
+
         # Load the report data from the JSON file and return a LighthouseReport object
         try:
             with open(self.report_path+".report.json", 'r') as fil:
@@ -183,7 +194,7 @@ class LighthouseRunner:
         """
         Clean up any temporary files created during the performance test.
         """
-        
+
         # Remove the JSON and HTML report files generated by Lighthouse
         os.remove(self.report_path+".report.json")
         os.remove(self.report_path+".report.html")
@@ -199,42 +210,58 @@ class LighthouseRepeatRunner:
             Both fields are dictionaries that map performance metric names to their average 
             value across all test repeats.
     """
-    
-    def __init__(self, url: str, form_factor: str = 'mobile', quiet: bool = True,
-                 additional_settings: List[str] = [], repeats: int = 3, timings:List[str]=BASE_TIMINGS):
-        """ 
+
+    def __init__(
+        self,
+        url: str,
+        form_factor: str = 'mobile',
+        quiet: bool = True,
+        chrome_flags: str='--headless --no-sandbox',
+        additional_settings: List[str] = [],
+        repeats: int = 3,
+        timings:List[str]=BASE_TIMINGS
+    ):
+        """
         Initialize a new LighthouseRepeatRunner object.
-        
+
         Args:
             url (str): The URL to test.
-            
+
             form_factor (str, optional): The form factor to use for the test. Either 'mobile' or 'desktop'.
                 Defaults to 'mobile'.
-                
+
+            chrome_flags (str, optional): A string of flags used to configure the Chrome runner.
+
             quiet (bool, optional): Whether to suppress output to stdout. Defaults to True.
-            
+
             additional_settings (list of str, optional): A list of additional parameters to use for the test.
                 Defaults to an empty list.
-                
+
             repeats (int, optional): The number of times to repeat the Lighthouse performance test. 
                 Defaults to 3.
-            
+
             timings (list, optional): A list of performance metrics to measure during the test.
                 Defaults to BASE_TIMINGS, which includes several common metrics.
         """
-        
+
         # Initialize an empty list to store the Lighthouse reports from each test repeat
         reports = []
-        
+
         # Use tqdm to display a progress bar while running the test repeats
         progress = tqdm(range(1, repeats + 1), desc='Repeating test')
 
         # Run the Lighthouse test the specified number of times and store the reports in a list
         for i in progress:
             progress.set_description('Run {0}/{1}'.format(i, repeats))
-            reports.append(LighthouseRunner(url, form_factor=form_factor,
-                                            quiet=quiet,
-                                            additional_settings=additional_settings,timings=timings).report) 
+            reports.append(LighthouseRunner(
+                url,
+                form_factor=form_factor,
+                quiet=quiet,
+                chrome_flags=chrome_flags,
+                additional_settings=additional_settings,
+                timings=timings
+                ).report
+            )
 
         # Calculate the average values for each performance metric across all test repeats
         report = namedtuple('LighthouseAveragedReport', 'timings, score')
@@ -246,17 +273,17 @@ class LighthouseRepeatRunner:
     def _get_average(self, obj_lst: List[Dict[str, Any]]) -> Dict[str, Any]:
         """
         Computes the average value for each performance metric across all Lighthouse reports.
-        
+
         Args:
             obj_lst (list of dict): A list of dictionaries, where each dictionary contains performance
                 metrics for a single Lighthouse report.
-        
+
         Returns:
             dict: A dictionary containing the average value for each performance metric across all reports. 
                 The keys of the dictionary are the names of the performance metrics, and the values are the 
                 average value of that metric across all reports.
         """
-        
+
         ret = {}
         for key in obj_lst[0].keys():
             lst = [x.get(key) for x in obj_lst]
@@ -267,26 +294,34 @@ class BatchRunner:
     """
     Runs Lighthouse reports for a batch of URLs and form factors.
     """
-    
-    def __init__(self, urls: List[str], form_factors: List[str], quiet: bool = True,
-                 additional_settings: dict = None, repeats: int = 3, timings:List[str]=BASE_TIMINGS):
+
+    def __init__(
+        self,
+        urls: List[str],
+        form_factors: List[str],
+        quiet: bool = True,
+        chrome_flags: str=DEFAULT_CHROME_FLAGS,
+        additional_settings: dict = None,
+        repeats: int = 3,
+        timings:List[str]=BASE_TIMINGS
+    ):
         """
         Args:
             urls (list(str)): List of URLs to run Lighthouse reports on.
-            
+
             form_factors (list(str)): List of form factors to run Lighthouse reports on.
-            
+
             quiet (bool, optional): Whether to suppress Lighthouse output. Defaults to True.
-            
+
             additional_settings (dict, optional): Additional Lighthouse settings to use for all reports.
                 Defaults to None.
-                
+
             repeats (int, optional): Number of times to repeat each report. Defaults to 3.
-            
+
             timings (list, optional): A list of performance metrics to measure during the test.
                 Defaults to BASE_TIMINGS, which includes several common metrics.
         """
-        
+
         # Ensure urls and form_factors are lists
         if not isinstance(form_factors, (list, )):
             form_factors = list(form_factors)
@@ -301,9 +336,14 @@ class BatchRunner:
 
         for url, factor in all_combinations:
             all_combinations.set_description('{0} | {1}'.format(url, factor))
-            
+
             # Run the Lighthouse report for this combination and append it to reports list
-            report = LighthouseRepeatRunner(url, factor, quiet,
-                                            additional_settings,
-                                            repeats=repeats,timings=timings).report
+            report = LighthouseRepeatRunner(
+                url,
+                factor,
+                quiet,
+                chrome_flags,
+                additional_settings,
+                repeats=repeats,timings=timings
+            ).report
             self.reports.append((url, factor, report))
